ce4d66dac2680f38d052a1852ff5d2359d44ca6d
WallacexMcCarthy <89401301+WallacexMcCarthy@users.noreply.github.com>
2022-09-10 08:27:41 -0600

    Initial Commit

---
+++ TimeTrackApp/TimeTrackAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimeTrackAppApp.swift
+//  TimeTrackApp
+//
+//  Created by Wallace McCarthy on 9/10/22.
+//
+
+import SwiftUI
+
+@main
+struct TimeTrackAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
