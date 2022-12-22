fbb1d7e846f8bbef4b25e8119942d7627ac0f781
WallacexMcCarthy <89401301+WallacexMcCarthy@users.noreply.github.com>
2022-12-22 14:54:59 -0700

    profile view changes

--- TimeTrackApp/UserViews/ProfileView.swift
+++ TimeTrackApp/UserViews/ProfileView.swift
@@ -14,26 +14,149 @@ struct ProfileConfirmationView: View
 {
     @State private var toProfileView = false
 //    @State private var attendanceInfo = getAttendanceInformation()
+    @State private var userData = loadCurrentUserData()
+    @State private var enteredPassword = ""
+    @State private var identityChecked = false
+    @State private var displayLoginScreen = false
+    @State private var displayFailure = false
+    @State public var fullName = ""
+    @State public var securityQuestion = ""
+    @State public var securityAnswer = ""
+    @State public var emailAddress = ""
+    @State public var password = ""
+    @State public var checkPassword = ""
+    @State public var grade = ""
+    @State public var birthdate = ""
+    @State public var school = ""
+    @State public var imageLink = ""
     var body: some View
     {
         if toProfileView
         {
             ProfileView(studentInfo: loadUserData()[0])
-        }else
+        }else if identityChecked
+        {
+            VStack
+            {
+                // Creates the text fields that the user can type in.
+                VStack
+                {
+                    TextField("Full Name", text: $fullName)
+                        .padding()
+                        .frame(width: 300, height: 50)
+                        .background(Color.black.opacity(0.07))
+                        .cornerRadius(10)
+                    TextField("Email Address", text: $emailAddress)
+                        .padding()
+                        .frame(width: 300, height: 50)
+                        .background(Color.black.opacity(0.07))
+                        .cornerRadius(10)
+                    TextField("Grade", text: $grade)
+                        .keyboardType(.numberPad)
+                        .padding()
+                        .frame(width: 300, height: 50)
+                        .background(Color.black.opacity(0.07))
+                        .cornerRadius(10)
+                }
+                TextField("Birthday (00/00/0000)", text: $birthdate)
+                    .padding()
+                    .frame(width: 300, height: 50)
+                    .background(Color.black.opacity(0.07))
+                    .cornerRadius(10)
+                TextField("School", text: $school)
+                    .padding()
+                    .frame(width: 300, height: 50)
+                    .background(Color.black.opacity(0.07))
+                    .cornerRadius(10)
+                TextField("Security Question", text: $securityQuestion)
+                    .padding()
+                    .frame(width: 300, height: 50)
+                    .background(Color.black.opacity(0.07))
+                    .cornerRadius(10)
+                TextField("Security Answer", text: $securityAnswer)
+                    .padding()
+                    .frame(width: 300, height: 50)
+                    .background(Color.black.opacity(0.07))
+                    .cornerRadius(10)
+                TextField("Password", text: $password)
+                    .padding()
+                    .frame(width: 300, height: 50)
+                    .background(Color.black.opacity(0.07))
+                    .cornerRadius(10)
+                TextField("Confirm Password", text: $checkPassword)
+                    .padding()
+                    .frame(width: 300, height: 50)
+                    .background(Color.black.opacity(0.07))
+                    .cornerRadius(10)
+                
+                VStack
+                {
+                    if displayFailure
+                    {
+                        Text("Passwords do not match!")
+                            .font(Font.title3)
+                            .foregroundColor(.red)
+                    }
+                    // the button that will bring the user to the confirmation create account view.
+                    Button("Create Account")
+                    {
+                        if checkPassword == password
+                        {
+                            appendData()
+                            
+                        }else
+                        {
+                            displayFailure = true
+                        }
+                    }
+                    .foregroundColor(.white)
+                    .frame(width: 300, height: 50)
+                    .background(Color.blue)
+                    .cornerRadius(10)
+                    
+                }
+            }
+        } else
         {
             NavigationView
             {
                 Form
                 {
-                    Text("tester")
+                    SecureField("Password", text: $enteredPassword)
+                        .padding()
+                        .frame(width: 300, height: 50)
+                        .background(Color.black.opacity(0.07))
+                        .cornerRadius(10)
+                        
                     Button("Confirm")
                     {
-                        toProfileView = true
+                        checkPassword(password: enteredPassword)
                     }
                 }
             }
         }
     
+    }
+    func checkPassword(password : String)
+    {
+        var userData = loadCurrentUserData()
+        
+        if(password == userData[0].password)
+        {
+            identityChecked = true
+        }
+    }
+    func appendData()
+    {
+        var userData = loadCurrentUserData()
+        
+//        userData[0].fullName = fullName
+//        userData[0].password = password
+//        userData[0].emailAddress = emailAddress
+//        userData[0].grade = grade
+//        userData[0].securityAnswer = securityAnswer
+//        userData[0].securityQuestion = securityQuestion
+//        userData[0].
     }
 }
 
